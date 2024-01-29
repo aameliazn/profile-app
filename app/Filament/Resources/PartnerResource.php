a4dd6d87be2adc630af93678a06ee94564ac9c1d
aameliazn <aameliazulkarnaen@gmail.com>
2024-01-29 13:36:26 +0700

    update icon partner and section admin

--- app/Filament/Resources/PartnerResource.php
+++ app/Filament/Resources/PartnerResource.php
@@ -20,7 +20,7 @@ class PartnerResource extends Resource
 {
     protected static ?string $model = Partner::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-external-link';
 
     public static function form(Form $form): Form
     {
